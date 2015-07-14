826308545daf6cf1936522fd9ba8c0e379811fde
vince <vince@aspendigital.com>
2015-07-14 15:50:10 -0600

    Fixed a bug in the script computing header image size

--- js/theme.js
+++ js/theme.js
@@ -27,8 +27,15 @@ var showOptions = {
 var backToTopVisible = false;
 
 $(function() {
-	$('.parallax').parallax();
 
+	var img = new Image();
+	img.onload = function() {
+		$('main').css('top', compute_main_top(img));
+	};
+
+	img.src = $('body').css('background-image').replace(/url\(|\)$|"/ig, '');
+
+	// Trigger focus styles for sidebar search
 	$('#sidebar-search').focus(function() {
 		var $this = $(this);
 		//$this.closest('div.input-field').addClass('z-depth-1');
@@ -39,6 +46,7 @@ $(function() {
 		$this.attr('placeholder', 'search this site...');
 	});
 
+	// Trigger navbar show search animation
 	$('#nav-search-link').click(function(e) {
 		e.preventDefault();
 		var $this = $(this);
@@ -46,22 +54,26 @@ $(function() {
 		$this.closest('ul').vivo(hideOptions);
 	});
 
+	// Trigger navbar hide search animation
 	$('#nav-search').focus(function(){
 		$('body, html').animate({scrollTop: 0});
 	}).focusout(function(e) {
 		$('#nav-search').vivo(hideOptions);
 	});
 
+	// Initialize side nav for mobile
 	$(".button-collapse").sideNav({
 		closeOnClick: true
 	});
 
+	// Initialize categories dropdown
 	$('.dropdown-button').dropdown({
 		constrain_width: false,
       	hover: true,
       	belowOrigin: true
 	});
 
+	// Trigger expand post function
 	$('.read-more-link').on('click', function(e) {
 		e.preventDefault();
 		var $excerpt = $(this).closest('div.cc-post-excerpt');
@@ -70,10 +82,12 @@ $(function() {
 		$more.show();
 	});
 
+	// Handle clicks on back-to-top button
 	$('#back-to-top-container').on('click', function(e) {
 		$('body, html').animate({scrollTop: 0});
 	});
 
+	// Fade in/out back-to-top button
 	$(window).scroll(function() {
 		var scroll = parseInt($(window).scrollTop());
 		if (scroll > 400)
@@ -91,15 +105,28 @@ $(function() {
 		}
 	});
 
+	// Trigger hover styles for rec'd post images
 	$('.recd-post-img').mouseover(function(){
 		$(this).animate({opacity: 0.70},{duration: 300});
 	}).mouseout(function(){
 		$(this).animate({opacity: 1},{duration: 300});
 	});
-/*
-	Materialize.scrollFire([{
-		selector: 	'main',
-		offset: 	400,
-		callback: 	""
-	}]);*/
 });
+
+function compute_main_top(img)
+{
+	var w = $(window).width();
+	var h = $(window).height();
+	var top = 0;
+
+	img_h = (w/img.width) * img.height;
+
+	if (w < 992)
+		img_h -= 56;
+	else
+		img_h -= 110;
+
+	top = Math.min(0.7 * h, img_h);
+	
+	return top + 'px';
+}
